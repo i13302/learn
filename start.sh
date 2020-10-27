6d1b670d315698ace43f06a5b4795845e142af1b
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2020-10-28 00:24:52 +0900

    fix pwd

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --publish 8888:8888 --volume `PWD`/work:/home/jovyan/work/ superml 
+docker run -it --rm --publish 8888:8888 --volume $(pwd)/work:/home/jovyan/work/ superml
