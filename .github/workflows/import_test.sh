00bb598bfe5b93e13bdd9e227b9138f77bee83e3
i13302 <s20g470@stu.kagawa-u.ac.jp>
2020-10-28 11:36:45 +0900

    fix command

--- .github/workflows/import_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 -c 'import graphviz'
-python3 -c 'import matplotlib'
-python3 -c 'import MeCab'
-python3 -c 'import numpy'
-python3 -c 'import pandas'
-python3 -c 'import seaborn'
-python3 -c 'import tensorflow'
